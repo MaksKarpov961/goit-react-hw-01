22914995c8b31c95ad8d3db402df1f349450d20a
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-10-18 16:34:57 +0300

    hm1_task-1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,53 @@
+import css from './Profile.module.css'
+import PropTypes from 'prop-types';
+
+
+
+export default function Profile({name, tag, location, image, stats}) {
+  return (
+    <div className= {css.profile_container}>
+      <div className= {css}>
+        <div className= {css.img_wrapper}>
+          <img className= {css.img}
+            src={image}
+            alt="User avatar"
+          />
+        </div>
+        <div className= {css.user_info_wrapper}>
+          <p className= {css.user_name}>{name}</p>
+          <p className= {css.user_email}>@{tag}</p>
+          <p className= {css.user_adress}>{location}</p>
+        </div>
+      </div>
+
+      <ul className= {css.stats_wrapper}>
+        <li className= {css.item}>
+          <span className= {css.stats_info_name}>Followers</span>
+          <span className= {css.stats_ifo_number}>{stats.followers}</span>
+        </li>
+        <li className= {css.item}>
+          <span className= {css.stats_info_name}>Views</span>
+          <span className= {css.stats_ifo_number}>{stats.views}</span>
+        </li>
+        <li className= {css.item}>
+          <span className= {css.stats_info_name}>Likes</span>
+          <span className= {css.stats_ifo_number}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+
+// Опис типів пропсів компонента 
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Імпорт стилів нормалізації
+import "modern-normalize";
+import App from "./components/App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
