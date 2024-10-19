be7d3c907c8287509c04ff0af8f9462c6ca87a37
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-10-19 22:10:22 +0300

    end task 3 and hw1

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,28 +1,25 @@
-import css from './TransactionHistory.module.css'
+import css from './TransactionHistory.module.css';
 
 export default function TransactionHistory({ items }) {
   return (
-    <table className= {css.table_histori}>
-  <thead>
-    <tr>
-      <th className= {css.table_cell}>Type</th>
-      <th className= {css.table_cell}>Amount</th>
-      <th className= {css.table_cell}>Currency</th>
-    </tr>
-  </thead>
+    <table className={css.table_histori}>
+      <thead className={css.table_thead}>
+        <tr>
+          <th className={css.table_cell_top}>Type</th>
+          <th className={css.table_cell_top}>Amount</th>
+          <th className={css.table_cell_top}>Currency</th>
+        </tr>
+      </thead>
 
       <tbody>
-        {items.map(item => {
-          return (
-            <tr key = {item.id} className= {css.table_row}>
-              <td className= {css.table_cell}>{item.type }</td>
-              <td className= {css.table_cell}>{item.amount}</td>
-              <td className= {css.table_cell}>{item.currency}</td>
-            </tr>
-          )
-        })}
-
-  </tbody>
-</table>
-  )
+        {items.map(item => (
+          <tr key={item.id} className={css.table_row}>
+            <td className={css.table_cell}>{item.type}</td>
+            <td className={css.table_cell}>{item.amount}</td>
+            <td className={css.table_cell}>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
